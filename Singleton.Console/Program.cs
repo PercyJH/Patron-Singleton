d7bf183f02141e2c4057c37662d954e8787f3520
Percy Herrera Valdez <percy_j98@hotmail.com>
2022-05-17 18:13:58 -0500

    first commit

---
+++ Singleton.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Singleton;
+using Singleton.Core;
+
+namespace Singleton.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Username = "Percy";
+            usuario.Password = "12345";
+            try
+            {
+                Manejador.Login(usuario);
+                Manejador u = Manejador.GetInstance;
+                Manejador.Logout();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Singleton.Core/Manejador.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Core
+{
+    public class Manejador
+    {
+        private static object _lock = new Object();
+        //4 guardamos desde una variable estatica la sesión a administrar
+        private static Manejador _session;
+        public Usuario Usuario { get; set; } //1 no se puede instanciar deliveradamente
+        public DateTime FechaInicio { get; set; }
+
+        //5 operacion estatica que nos permite acceder a esta instancia global
+        public static Manejador GetInstance
+        {
+            get //6 acceso a la instancia
+            {
+                if (_session == null) throw new Exception("Sesión no iniciada");
+                //7 si sesion es nulo crea una nueva sesion si no, nos devuelve la sesion actual
+                return _session;
+            }
+        }
+        //8 como este es un manejador de sesion definimos algunas funciones
+        public static void Login(Usuario usuario)
+        {//9 nos asegura la creacion de una instancia y mantenerla
+            lock (_lock)
+            {
+                //control del objeto sesion
+                if (_session == null)
+                {
+                    _session = new Manejador();
+                    _session.Usuario = usuario;
+                    _session.FechaInicio = DateTime.Now;
+                }// al entrar una nueva sesion siempre va a preguntar si hay
+                 // una anterior lo cual puede ser un problema
+                else
+                {
+                    throw new Exception("Sesion iniciada");
+                }
+            }
+        } //10 aqui se encapsula el acceso a la instancia de manera global
+        public static void Logout()
+        {
+            lock (_lock)
+            {
+                if (_session != null)
+                {
+                    _session = null;
+                }
+                else
+                {
+                    throw new Exception("Sesión no iniciada ");
+                }
+            }
+        }
+        private Manejador() //2 constructor privado
+                            //3 se garantiza que la clase manejador no se puede hacer "new" desde cualquier lugar
+        {
+        }
+    }
+}
